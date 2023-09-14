efbe8572a49b05776d10c65db339644b2e0ab7d0
parsTroy <troyparsons@hotmail.com>
2023-09-14 13:15:16 -0400

    Remove current pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import Footer from "@/global/Footer";
-import Navbar from "@/global/Navbar";
-
 
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Footer />
     </>
   )
 }
