b46afc89112e60ca81f3a9e410ba9973563ff311
parsTroy <troyparsons@hotmail.com>
2023-09-16 17:44:37 -0400

    Update mobile side bar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { Montserrat } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { LayoutDashboard, LucideBookMarked, LucideCalendarDays, LucideGlasses, LucideGraduationCap, LucideLineChart, LucidePieChart } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 
 const montserrat = Montserrat({weight: '600', subsets: ['latin']});
 
@@ -47,7 +48,10 @@ const routes = [
     },
 ];
 
-const sidebar = () => {
+const Sidebar = () => {
+
+    const pathname = usePathname();
+
   return (
     <div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
         <div className='px-3 py-2 flex-1'>
@@ -61,7 +65,7 @@ const sidebar = () => {
             </Link>
             <div className='space-y-1'>
                 {routes.map((route) => (
-                    <Link href={route.href} key={route.href} className='text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition'>
+                    <Link href={route.href} key={route.href} className={cn('text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition', pathname === route.href ? "text-white bg-white/10" : "text-zinc-400" )}>
                         <div className='flex items-center flex-1'>
                             <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
                             {route.label}
@@ -74,4 +78,4 @@ const sidebar = () => {
   )
 }
 
-export default sidebar
+export default Sidebar
