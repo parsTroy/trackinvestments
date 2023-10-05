1fce684ac419de68d5b5b6b1d6bb3a4e4054d3a9
Troy <troyparsons@hotmail.com>
2023-10-05 15:38:46 +0900

    Update dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,45 +2,38 @@
 
 import { Card } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
-import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from 'lucide-react';
+import { ArrowRight, Banknote, CircleDollarSign, DollarSign, LineChart, VideoIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const tools = [
   {
-    label: "Conversation",
-    icon: MessageSquare,
+    label: "Portfolio Yield",
+    icon: CircleDollarSign,
     color: "text-violet-500",
     bgColor: "bg-violet-500/10",
     href: "/conversation"
   },
   {
-    label: "Music",
-    icon: Music,
+    label: "Yield On Cost",
+    icon: DollarSign,
     color: "text-emerald-500",
     bgColor: "bg-emerald-500/10",
     href: "/music"
   },
   {
-    label: "Image",
-    icon: ImageIcon,
+    label: "Annual Income",
+    icon: Banknote,
     color: "text-pink-700",
     bgColor: "bg-pink-700/10",
     href: "/image"
   },
   {
-    label: "Video",
-    icon: VideoIcon,
+    label: "Portfolio Beta",
+    icon: LineChart,
     color: "text-orange-700",
     bgColor: "bg-orange-700/10",
     href: "/video"
-  },
-  {
-    label: "Code",
-    icon: Code,
-    color: "text-green-700",
-    bgColor: "bg-green-700/10",
-    href: "/code"
   },
 ]
 
@@ -51,9 +44,10 @@ const Dashboard = () => {
   return (
     <div>
       <div className='mb-8 space-y-4'>
-        <h2 className='text-2xl md:text-4xl font-bold text-center'>$20,986</h2>
+        <h2 className='text-2xl md:text-4xl font-bold text-center'>$160,986</h2>
         <p className='text-muted-foreground font-light text-sm md:text-lg text-center'>Summary ACCOUNTNAME + Add Account</p>
       </div>
+
       <div className='px-4 md:px-20 lg:px-32 space-y-4'>
         {tools.map((tool) => (
           <Card onClick={() => router.push(tool.href)} key={tool.href} className='p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer'>
